aa74454e4cb3ebda106b181853d9e891b12a9372
KeemoKimo <thebunthong@gmail.com>
2022-05-21 09:04:24 +0700

    Learned how to make class and object in javascript

--- mainPage.js
+++ mainPage.js
@@ -1,3 +1,14 @@
+class Person{
+    constructor(name, age, nationality){
+        this.name = name;
+        this.age = age;
+        this.nationality = nationality;
+    }
+}
+
+var bunthong = new Person("Bunthong Kong", 15, "Cambodia");
+var chhayly = new Person("Chhayly Sreng", 16, "German");
+
 function popUpFunc(){
-    window.alert("Hello motherfrickers!");
+    window.alert(bunthong.name + " / " + chhayly.nationality);
 }
