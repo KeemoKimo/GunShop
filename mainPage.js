044fda8fbfdf431316ae74d80439c6335f6c6002
KeemoKimo <thebunthong@gmail.com>
2022-05-21 11:32:28 +0700

    Functions testing
    learned how to test out function and link them with html.

--- mainPage.js
+++ mainPage.js
@@ -1,3 +1,4 @@
+
 class Person{
     constructor(name, age, nationality){
         this.name = name;
@@ -9,6 +10,18 @@ class Person{
 var bunthong = new Person("Bunthong Kong", 15, "Cambodia");
 var chhayly = new Person("Chhayly Sreng", 16, "German");
 
-function popUpFunc(){
-    window.alert(bunthong.name + " / " + chhayly.nationality);
+function saveData(){
+    var text = document.getElementById("theName").value;
+    if(text!=""){
+        window.alert("Hello, and welcome back " + text);
+    }else if(text==""){
+        window.alert("Please enter this field!");
+    }
 }
+
+function clearTxt(){
+    document.getElementById("theName").value = "";
+}
+
+
+
